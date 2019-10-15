135fb024e2e0ae7be3479129db773de2ef5bf42f
gaoruixue1215 <1171331232@qq.com>
2019-10-15 08:58:25 +0800

    add

---
+++ build/src/Todolist/Todoing.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+export default class Todoing extends Component {
+    render() {
+        return (
+            <div>
+                <h1>正在进行</h1>
+                <ul>
+                    {
+                        this.props.todo.map((item,idx)=><li key={idx}>{item}----<button onClick={()=>{this.props.delTodo(idx)}}>删除</button></li>)
+                    }
+                </ul>
+            </div>
+        )
+    }
+}
+Toding.propTypes = {
+    todo:PropTypes.array
+}
+Todoing.defaultPimport React, { Component } from 'react'
+import PropTypes from 'prop-types';
+export default class Todoing extends Component {
+    render() {
+        return (
+            <div>
+                <h1>正在进行{this.props.a}</h1>
+                <ul className="list">
+                    {
+                        this.props.todo.map((item,idx)=><li key={idx}>{item}----<button onClick={()=>{this.props.delTodo(idx)}}>删除</button></li>)
+                    }
+                </ul>
+            </div>
+        )
+    }
+}
+Todoing.propTypes = {
+    todo: PropTypes.array
+}
+Todoing.defaultProps = {
+    todo: [2,3,4],
+    a: 100
+}
